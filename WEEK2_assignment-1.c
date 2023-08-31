8628059c993fd559b809145f7655cbadb39b16dd
R.SHYAM <shyamramesh2002@gmail.com>
2023-08-31 17:04:15 +0530

    Create  week3-assignment1.c
    Write a C Program to calculates the area (floating point number with two decimal places) of a Circle given it’s radius (integer value).  The value of Pi is 3.14.

---
+++ WEEK2_assignment-1.c
@@ -0,0 +1,15 @@
+/* Week 3 : Programming Assignment 1*/
+#include <stdio.h>
+#define PI 3.14
+void main()
+{
+    int radius;
+    float area;
+    /* Enter the radius of a circle */
+    scanf("%d", &radius);
+
+area = PI * radius * radius;  
+in the space provided */
+area = PI*radius*radius;
+printf("Area of a circle = %5.2f", area);
+}
