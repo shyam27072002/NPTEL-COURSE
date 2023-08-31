7206aed61077816bf44dc40ed161a9f83987e959
R.SHYAM <shyamramesh2002@gmail.com>
2023-08-31 17:09:38 +0530

    Create week4-assignment1.c

---
+++ week4-assignment1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int n1, n2, n3; 
+    scanf("%d %d %d", &n1, &n2, &n3); 
+if (n1<n2)
+    {
+        if(n1<n3)
+            printf("%d is the smallest number.", n1);
+        else
+            printf("%d is the smallest number.", n3);
+    }
+    else
+    {
+        if(n2<n3)
+            printf("%d is the smallest number.", n2);
+        else
+            printf("%d is the smallest number.", n3);
+    }
+}
